1b30f4a1dabbc6233f7eeacfa0316227e0159557
David Fallow <dfallow@protonmail.com>
2023-07-24 10:42:52 +0300

    add nudge layout completed

--- components/AddNudge.tsx
+++ components/AddNudge.tsx
@@ -1,12 +1,16 @@
 import React, { Component, useState } from "react";
-import { View, Text, Button, StyleSheet, Dimensions, FlatList, Image } from "react-native";
+import { View, Text, Button, StyleSheet, Dimensions, FlatList, Image, TouchableOpacity } from "react-native";
 import { SelectList } from 'react-native-dropdown-select-list'
 import Carousel from 'react-native-snap-carousel';
+import { tempContacts } from "../utils/static/lists";
 
 const AddNudge = ({onClose}: any) => { 
 
+    const allContacts = tempContacts();
+    const [contacts, setContacts] = useState(allContacts);
+    const [selectedContact, setSelectedContact] = useState<string>("");
     const [selectedIndex, setSelectedIndex] = useState("");
-    const width = Dimensions.get('window').width;
+    const [startDate, setStartDate] = useState<Date>(new Date());
 
     const options = [
         { key: '1', value: 'Daily'},
@@ -15,26 +19,52 @@ const AddNudge = ({onClose}: any) => {
         { key: '3', value: 'Monthly'},
     ]
 
-    interface Contact {
-        id: string,
-        name: string,
-        img: any,
+
+
+    const setContact = (contact: any) => {
+        console.log(contact);
+        selectedContact === contact.id ? setSelectedContact("") : setSelectedContact(contact.id);
     }
 
-    const contacts = [
-        {id: '1', name: 'John', img: require('../assets/dinosaur.jpg')},
-        {id: '2', name: 'Steve', img: require('../assets/dinosaur.jpg')},
-        {id: '3', name: 'Dave', img: require('../assets/dinosaur.jpg')},
-        {id: '4', name: 'Tom', img: require('../assets/dinosaur.jpg')},
-    ]
+    const dateAndFrequency = (frequency: string) => {
+        console.log(frequency);
+        setSelectedIndex(frequency);
 
-    const renderContact = ({contact}: any) => {
-        console.log("Contacts", contacts)
-        console.log(contact)
+        const today= new Date();
+
+        switch(frequency) {
+            case "Daily":
+               startDate.setDate(today.getDate() + 1);
+               console.log(startDate);
+               break;
+            case "Weekly":
+                startDate.setDate(today.getDate() + 7);
+                break;
+            case "Bi-Monthly":
+                startDate.setDate(today.getDate() + 14);
+                break;
+            case "Monthly":
+                startDate.setDate(today.getDate() + 30);
+                break;
+            default:
+                console.log("No frequency selected");
+        }
+    }
+
+    const renderItem = ({item}: any) => {
         return(
-            <View>
-                {contact && <Image source={contact.image} />}
-            </View>
+            item && (
+                <View style={styles.contactBox}>
+                    <TouchableOpacity onPress={() => setContact(item)}>
+                        <Image 
+                            style={selectedContact === item.id ? [styles.contactImg, styles.contactSelected] : styles.contactImg}
+                            source={item.img} />
+                    </TouchableOpacity>
+        
+                    <Text>{item.name}</Text>
+                </View>
+            )
+            
         )
         
     }
@@ -42,22 +72,23 @@ const AddNudge = ({onClose}: any) => {
     return (
         <View style={styles.modal}>
 
-            <FlatList
+            <View style={styles.contactList}>
+                <FlatList
                 data={contacts}
-                renderItem={renderContact}
-                keyExtractor={(item) => item.id}
+                renderItem={renderItem}
                 horizontal={true}
-            />
+                />
+            </View>
             
-            <View style={styles.modalBox}>
+            <View style={styles.frequencyBox}>
 
-                <View style={styles.modalStatic}>
-                    <Text>Frequency</Text>
+                <View>
+                    <Text style={styles.frequencyText}>Frequency:</Text>
                 </View>
 
                 <View style={styles.modalChoices}>  
                     <SelectList
-                        setSelected={(option: any) => setSelectedIndex(option)}
+                        setSelected={dateAndFrequency}
                         data={options}
                         save="value"
                         search={false}
@@ -66,8 +97,17 @@ const AddNudge = ({onClose}: any) => {
 
             </View>
 
+            <View style={styles.nextDateBox}>
+
+                <Text style={styles.frequencyText}>Next Date:</Text>
+
+                <Text style={styles.frequencyText}>{startDate.toDateString()}</Text>
+            </View>
+
             <View style={styles.confirm}>
-                <Text>Add Nudge</Text>
+                <TouchableOpacity style={styles.confirmBtn}>
+                    <Text>Confirm</Text>
+                </TouchableOpacity>
                 <Button
                     title="Close"
                     onPress={onClose}
@@ -82,7 +122,7 @@ export default AddNudge;
 
 const styles = StyleSheet.create({
     modal: {
-        borderWidth: 1,
+        borderWidth: 0,
         flexDirection: 'column',
         width: "95%",
         height: "85%",
@@ -94,15 +134,56 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-around',
     },
-    modalStatic: {
-        flexDirection: 'column',
-    },
     modalChoices: {
         flexDirection: 'column',
     },
     confirm: {
         flex: 1,
-        borderWidth: 1,
+        borderWidth: 0,
+        width: '50%',
         justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    contactList: {
+        height: 100,
+        marginBottom: 10,
+    },
+    contactBox: {
+        alignItems: 'center',
+        height: 100,
+    },
+    contactImg: {
+        height: 80,
+        aspectRatio: 1,
+        borderRadius: 100,
+        marginHorizontal: 5,
+    },
+    contactSelected: {
+        borderWidth: 2,
+    },
+    frequencyBox: {
+        marginVertical: 5,
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+    frequencyText: {
+        fontSize: 20,
+    },
+    nextDateBox: {
+        width: '100%',
+        marginVertical: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+    confirmBtn: {
+        borderWidth: 1,
+        borderRadius: 15,
+        backgroundColor: '#fff',
+        width: '50%',
+        height: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 })
---
+++ utils/static/lists.ts
@@ -0,0 +1,9 @@
+export const tempContacts = () => {
+    const contacts = [
+        {id: '1', name: 'John', img: require('../..//assets/dinosaur.jpg')},
+        {id: '2', name: 'Steve', img: require('../../assets/dinosaur.jpg')},
+        {id: '3', name: 'Dave', img: require('../../assets/dinosaur.jpg')},
+        {id: '4', name: 'Tom', img: require('../../assets/dinosaur.jpg')},
+    ]
+    return contacts;
+}
